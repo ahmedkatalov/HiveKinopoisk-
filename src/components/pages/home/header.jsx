82012ec7a81724f4e1b93ba1c273343b21c7cf66
Madlenis <arcuspluvius17@gmail.com>
2024-09-23 17:50:33 +0300

    hello again

--- src/components/pages/home/header.jsx
+++ src/components/pages/home/header.jsx
@@ -1,1 +1,17 @@
-//hello
+import './header.css';
+
+const Header = () => {
+    return (
+        <header className="header-box">
+            <div className="header-content">
+                <h2 className="headline">M<span className="h2-outstanding-letters">oo</span>vie</h2>
+                <input className="input-search" type="text" placeholder="Search" />
+                <a className="h-link active">Home</a>
+                <a className="h-link active">My list</a>
+                <button className="sign-in-btn" type="button">Sign in</button>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
