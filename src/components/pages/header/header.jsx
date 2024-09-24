6a3fb4abeaabac6a06784fb05779fb56a3b61564
ahmedkatalov <148577116+ahmedkatalov@users.noreply.github.com>
2024-09-24 22:04:01 +0300

    Merged conflicts

--- src/components/pages/header/header.jsx
+++ src/components/pages/header/header.jsx
@@ -1,4 +1,4 @@
-import './header.css';
+import './Header.css';
 
 const Header = () => {
     return (
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="header-content">
                 <h2 className="headline">M<span className="h2-outstanding-letters">oo</span>vie</h2>
                 <input className="input-search" type="text" placeholder="Search" />
-                <a className="h-link active">Home</a>
+                <a href='../Carousel.jsx' target='_blank' className="h-link active">Home</a>
                 <a className="h-link active">My list</a>
                 <button className="sign-in-btn" type="button">Sign in</button>
             </div>
