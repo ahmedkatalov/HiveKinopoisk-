e2b88ecbafc1a1739f64f9036c5625c541af9c9e
ahmedkatalov <148577116+ahmedkatalov@users.noreply.github.com>
2024-09-24 19:38:31 +0300

    Update store.js

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -3,7 +3,7 @@ import moviesReducer from './searchMovies';
 import favoriteReducer from './favoriteMovies';
 import genreMovie from './genreMovie';
 
-// Создаём хранилище
+
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
